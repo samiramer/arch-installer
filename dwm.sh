5af44c4421d7f19c8fd6bd8c3d59ebaae10143cf
samer <samirmamer@gmail.com>
2021-09-03 23:01:42 -0400

    Add the rust package

--- dwm.sh
+++ dwm.sh
@@ -4,15 +4,15 @@ sudo timedatectl set-ntp true
 sudo hwclock --systohc
 sudo reflector -c Canada -a 4 --sort rate --save /etc/pacman.d/mirrorlist
 
-cd /tmp
-git clone https://aur.archlinux.org/paru.git
-cd paru/;makepkg -si --noconfirm;cd
-
 # Install packages
-sudo pacman -S xorg polkit-gnome feh lxappearance pcmanfm
+sudo pacman -S xorg polkit-gnome feh lxappearance pcmanfm rust
 
 # Install fonts
 sudo pacman -S --noconfirm nerd-fonts-hack cbatticon network-manager-applet zsh-autosuggestions zsh-syntax-highlighting
+
+cd /tmp
+git clone https://aur.archlinux.org/paru.git
+cd paru/;makepkg -si --noconfirm;cd
 
 # Pull Git repositories and install
 cd /tmp
