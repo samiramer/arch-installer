d9bdce7b78b85a3bd52c7e958a537f3f43a6e40e
samer <samirmamer@gmail.com>
2021-09-11 14:44:05 -0400

    Use alacritty instead of ST

--- install-dwm.sh
+++ install-dwm.sh
@@ -5,7 +5,7 @@ sudo hwclock --systohc
 sudo reflector --verbose -c Canada -a 4 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Install packages
-sudo pacman -Sy --noconfirm xorg-server libx11 libxft xorg-xinit xorg-xset xorg-xbacklight xorg-xsetroot gnome-keyring polkit-gnome feh lxappearance pcmanfm neovim tmux npm nodejs cbatticon network-manager-applet zsh-autosuggestions zsh-syntax-highlighting openfortivpn dunst pavucontrol
+sudo pacman -Sy --noconfirm xorg-server libx11 libxft xorg-xinit xorg-xset xorg-xbacklight xorg-xsetroot gnome-keyring polkit-gnome feh lxappearance pcmanfm neovim tmux npm nodejs cbatticon network-manager-applet zsh-autosuggestions zsh-syntax-highlighting openfortivpn dunst pavucontrol flameshot alacritty
 
 cd /tmp
 git clone https://aur.archlinux.org/yay-git.git
@@ -18,7 +18,7 @@ mkdir -p $HOME/media/{documents,downloads}
 
 # Pull Git repositories and install
 cd $HOME/media/code/personal
-repos=( "dmenu" "dwm" "st" )
+repos=( "dmenu" "dwm" )
 for repo in ${repos[@]}
 do
     git clone https://github.com/samiramer/$repo.git
