74f75727176acfd5a7892e9d5539901b52c57c30
samer <samirmamer@gmail.com>
2021-10-15 06:50:48 -0400

    Do some cleanup with paths.  Also include some packages I need

--- install-dwm.sh
+++ install-dwm.sh
@@ -5,33 +5,42 @@ sudo hwclock --systohc
 sudo reflector --verbose -c Canada -a 4 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Install packages
-sudo pacman -Sy --noconfirm xorg-server libx11 libxft xorg-xinit xorg-xset xorg-xbacklight xorg-xrandr xorg-xsetroot gnome-keyring polkit-gnome feh lxappearance pcmanfm neovim tmux npm nodejs cbatticon network-manager-applet zsh-autosuggestions zsh-syntax-highlighting openfortivpn dunst pavucontrol flameshot alacritty redshift
+sudo pacman -Sy --noconfirm xorg-server libx11 libxft xorg-xinit xorg-xset xorg-xrandr xorg-xsetroot feh lxappearance pcmanfm neovim tmux npm nodejs cbatticon network-manager-applet dunst pavucontrol flameshot alacritty redshift picom arandr autorandr
 
 cd /tmp
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git/;makepkg -si --noconfirm
 
-yay -S noto-fonts nerd-fonts-noto xss-lock betterlockscreen xclip
+yay -S noto-fonts nerd-fonts-source-code-pro xss-lock betterlockscreen xclip brave-bin
 
-mkdir -p $HOME/media/code/personal
-mkdir -p $HOME/media/{documents,downloads}
+mkdir -p $HOME/code/personal
+sleep 3
+mkdir -p $HOME/{documents,downloads}
+sleep 3
 
 # Pull Git repositories and install
-cd $HOME/media/code/personal
+cd $HOME/code/personal
+sleep 3
 repos=( "dmenu" "dwm" )
 for repo in ${repos[@]}
 do
     git clone https://github.com/samiramer/$repo.git
-    cd $HOME/media/code/personal/$repo;make;sudo make install;cd $HOME/media/code/personal
+    cd $HOME/code/personal/$repo;make;sudo make install;cd $HOME/code/personal
+    sleep 3
 done
 
-cd /home/samer/media/code/personal
+cd /home/samer/code/personal
 git clone https://github.com/samiramer/dot-files.git
 cd dot-files;./install.sh
+sleep 3
 
 betterlockscreen -u $HOME/.config/wall.jpg
 
+sleep 3
+
 sh -c "$(curl -vfsSL https://starship.rs/install.sh)" -- --verbose --yes --bin-dir $HOME/.local/bin
+
+sleep 3
 
 printf "\e[1;32mDone! you can now reboot.\e[0m\n"
 
