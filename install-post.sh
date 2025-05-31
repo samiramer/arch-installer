da7bf98aef81e7c33108b806825a3a2b9dbf17d4
Samir Amer <samir@sunnybyte.com>
2025-05-30 23:32:38 -0400

    Implement post install script that will run after reboot

---
+++ install-post.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd $CODEDIR
+repos=( "dmenu" "dwm" "dwmblocks" )
+for repo in ${repos[@]}
+do
+    git clone https://github.com/samiramer/$repo.git
+    cd $CODEDIR/$repo;make;sudo make clean install;cd $PERSONALDIR
+    sleep 1
+done
+
+# install dot files
+git clone https://github.com/samiramer/dot-files.git $CODEDIR/dot-files
+cd $CODEDIR/dot-files
+stow -t /home/samer alacritty fontconfig gitconfig scripts tmux ssh xinit zsh
+
+# install Neovim config
+git clone https://github.com/samiramer/neovim-config $CODEDIR/neovim-config
+ln -s $CODEDIR/neovim-config /home/samer
+
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm /home/samer/.tmux/plugins/tpm
+
+# install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
--- install.sh
+++ install.sh
@@ -52,30 +52,3 @@ echo "samer ALL=(ALL) ALL" >> /etc/sudoers.d/samer
 $CODEDIR="/files/code/personal"
 mkdir -p $CODEDIR
 chown -R samer:samer /files
-
-# switch to samer user so we can install some stuff
-su samer
-
-cd $CODEDIR
-repos=( "dmenu" "dwm" "dwmblocks" )
-for repo in ${repos[@]}
-do
-    git clone https://github.com/samiramer/$repo.git
-    cd $CODEDIR/$repo;make;sudo make clean install;cd $PERSONALDIR
-    sleep 1
-done
-
-# install dot files
-git clone https://github.com/samiramer/dot-files.git $CODEDIR/dot-files
-cd $CODEDIR/dot-files
-stow -t /home/samer alacritty fontconfig gitconfig scripts tmux ssh xinit zsh
-
-# install Neovim config
-git clone https://github.com/samiramer/neovim-config $CODEDIR/neovim-config
-ln -s $CODEDIR/neovim-config /home/samer
-
-# install tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm /home/samer/.tmux/plugins/tpm
-
-# install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
