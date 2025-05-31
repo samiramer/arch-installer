31f1d640873c324079b76a5794a7d87be83dc530
Samir Amer <samir@sunnybyte.com>
2025-05-30 22:32:37 -0400

    Write new install script, update README

--- README.md
+++ README.md
@@ -6,41 +6,40 @@
 2. Boot into the Archlinux iso
 3. Once booted execute the following command `timedatectl set-ntp true`
 4. Find the name of the device to install Archlinux on using `lsblk`
-5. Start partitioning the installation disk using `gdisk /dev/[device_name]`
+5. Start partitioning the installation disk using `cfdisk /dev/[device_name]`
 6. Partitioning scheme
 ```
-/efi -> size = 500MB, type = ef00 - EFI system partition
-/ -> size = type = 8304 - Linux x86-64 root (/)
-/home -> type = 8302 - Linux /home
+/boot -> size = 1G, type = EFI system partition
+/ -> size = type = Linux x86-64 root (/)
+/home -> type = Linux /home
+/files -> type = Linux filesystem
 ```
 7. Once partitioned, run `lsblk` to see the partitions and format them accordingly
 ```
 mkfs.fat -F32 /dev/[efi_partition]
 mkfs.ext4 /dev/[root_partition]
 mkfs.ext4 /dev/[home_partition]
+mkfs.ext4 /dev/[files_partition]
 ```
 8. Mount the partitions to /mnt
 ```
 mount /dev/[root-partition] /mnt
-mkdir /mnt/{efi,home}
-mount /dev/[efi_partition] /mnt/efi
+mkdir -p /mnt/{boot,home}
+mount /dev/[efi_partition] /mnt/boot
 mount /dev/[home_partition] /mnt/home
 ```
 9. Install the base linux packages
 ```
-pacstrap /mnt base linux linux-firmware vim git intel-ucode
+pacstrap /mnt base linux linux-firmware git amd-ucode
 ```
-> Install amd-ucode instead of intel-ucode if it's an AMD system
+> Install intel-ucode instead of amd-ucode if it's an Intel system
 10. Generate the fstab file
 ```
 genfstab -U /mnt > /mnt/etc/fstab
 ```
 11. Chroot into the /mnt folder - `arch-chroot /mnt`
 12. Clone the arch-installer repo - `cd /; git clone https://github.com/samiramer/arch-installer`
-13. Run the install-base.sh script - `cd /arch-installer; ./install-baase.sh`
+13. Run the install.sh script - `cd /arch-installer; ./install.sh`
 14. Once finished, exit chroot - `exit`
 15. Unmount all drives and reboot - `umount -a; reboot now`. The system will reboot and boot into the new system
-16. Login, you will get a zsh welcome prompt, press 'q' to "quit and do nothing"
-17. Run the dwm install script, the sudo password is "password" (change the user password later!) - `cd /arch-installer; ./install-dwm.sh`
-18. Reboot again - `sudo reboot now`
-19. Login and enjoy!
+16. Login and enjoy
---
+++ install.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+# setup network, locale and clock stuff
+ln -sf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
+hwclock --systohc
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-7" >> /etc/locale.conf
+echo "minicell" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 mini.localdomain mini" >> /etc/hosts
+echo root:password | chpasswd
+
+# install required packages
+pacman -Sy --noconfirm \
+    alacritty \
+    base-devel \
+    blueman \
+    bluez \
+    bluez-utils \ 
+    curl \
+    docker \
+    dunst \
+    efibootmgr \
+    feh \
+    flameshot \
+    fzf \
+    gnome-keyring \
+    gnome-themes-extra \
+    gnupg \
+    grub  \
+    i3lock \
+    imagemagick \
+    lazygit \
+    libnotify \
+    libsecret \
+    libx11 \
+    libxft \
+    lxappearance \
+    man-db \
+    man-pages \
+    mkcert \
+    neovim \
+    networkmanager \
+    networkmanager-openvpn \
+    network-manager-applet \
+    openssh \
+    openvpn \
+    os-prober \
+    pamixer \
+    pass \
+    pavucontrol \
+    pcmanfm \
+    pipewire \
+    pipewire-alsa \
+    pipewire-pulse \
+    polkit \
+    polkit-gnome \
+    ripgrep \
+    seahorse \
+    secret-tool \
+    sshd \
+    stow \
+    sudo \
+    texinfo \
+    tmux \
+    wget \
+    xautolock \
+    xdg-user-dirs \
+    xclip \
+    xdotool \
+    xorg \
+    xorg-xinit \
+    xsel \
+    xss-lock \
+    zip \
+    zsh \
+    unzip
+    
+# install local root CA
+mkcert -install
+
+# install yay
+cd /tmp
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git/;makepkg -si --noconfirm
+sleep 2
+
+# install extra packages
+yay -Sy --noconfirm \
+    picom-ftlabs-git \
+    ttf-adwaita-mono-nerd \
+    xautolock
+
+# setup grub
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+echo 'GRUB_CMDLINE_LINUX_DEFAULT="usbcore.autosuspend=-1 loglevel=3"' >> /etc/default/grub
+echo "GRUB_TIMEOUT=3" >> /etc/default/grub
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# enable system services
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable sshd
+systemctl enable docker
+
+# setup samer
+useradd -m -s /usr/bin/zsh samer
+echo samer:password | chpasswd
+usermod -aG docker samer
+chown -R samer:samer /home/samer
+echo "samer ALL=(ALL) ALL" >> /etc/sudoers.d/samer
+
+
+# prep the code folder
+$CODEDIR="/files/code/personal"
+mkdir -p $CODEDIR
+chown -R samer:samer /files
+
+# switch to samer user so we can install some stuff
+su samer
+
+cd $CODEDIR
+repos=( "dmenu" "dwm" "dwmblocks" )
+for repo in ${repos[@]}
+do
+    git clone https://github.com/samiramer/$repo.git
+    cd $CODEDIR/$repo;make;sudo make clean install;cd $PERSONALDIR
+    sleep 1
+done
+
+# install dot files
+git clone https://github.com/samiramer/dot-files.git $CODEDIR/dot-files
+cd $CODEDIR/dot-files
+stow -t /home/samer alacritty fontconfig gitconfig scripts tmux ssh xinit zsh
+
+# install Neovim config
+git clone https://github.com/samiramer/neovim-config $CODEDIR/neovim-config
+ln -s $CODEDIR/neovim-config /home/samer
+
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm /home/samer/.tmux/plugins/tpm
+
+# install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
